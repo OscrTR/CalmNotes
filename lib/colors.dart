ff49c0adaaaf2fe08ca61870bd47911b7166a235
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-07-24 11:56:28 +0200

    theme

---
+++ lib/colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primaryColor = Color(0xff444444);
+  static const Color secondaryColor = Color(0xffc1bdba);
+  static const Color ternaryColor = Color(0xff8a9193);
+  static const Color white = Color(0xffffffff);
+  static const Color backgroundColor = Color(0xfff7f2ed);
+  static const Color color0 = Color(0xfffedac2);
+  static const Color color1 = Color(0xffffe4ca);
+  static const Color color2 = Color(0xffffe7c8);
+  static const Color color3 = Color(0xfffee9c4);
+  static const Color color4 = Color(0xfffeecc2);
+  static const Color color5 = Color(0xfffeefbf);
+  static const Color color6 = Color(0xfff6efc2);
+  static const Color color7 = Color(0xffebefc4);
+  static const Color color8 = Color(0xffe4f0c6);
+  static const Color color9 = Color(0xffdcf0c9);
+  static const Color color10 = Color(0xffc9eec0);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,10 +10,56 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+      theme: ThemeData(
+        scaffoldBackgroundColor: AppColors.backgroundColor,
+        fontFamily: 'Inter',
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(
+            fontSize: 14,
+            color: AppColors.primaryColor,
+          ),
+          titleMedium: TextStyle(
+            fontSize: 14,
+            color: AppColors.primaryColor,
+            fontWeight: FontWeight.bold,
+          ),
+          headlineMedium: TextStyle(
+            color: AppColors.primaryColor,
+            fontFamily: 'PlayfairDisplay',
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Regular Inter text.',
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            Text(
+              'Bold Inter text.',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            Text(
+              'Bold PlayfairDisplay text.',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
     );
