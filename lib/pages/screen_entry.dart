0c32bb92f6b658e34c7208de47410aed4f891ef1
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-07-29 13:35:40 +0200

    save style

--- lib/pages/screen_entry.dart
+++ lib/pages/screen_entry.dart
@@ -72,111 +72,107 @@ class _ScreenEntryState extends State<ScreenEntry> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Text(
-                'How do you feel?',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-              Row(
-                children: [
-                  TextButton(
-                    onPressed: () => selectDate(context),
-                    child: Text(
-                      dateFormatter.format(selectedDate),
-                      style: Theme.of(context).textTheme.titleMedium,
-                    ),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              'How do you feel?',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Row(
+              children: [
+                TextButton(
+                  onPressed: () => selectDate(context),
+                  child: Text(
+                    dateFormatter.format(selectedDate),
+                    style: Theme.of(context).textTheme.titleMedium,
                   ),
-                  const Text(' - '),
-                  TextButton(
-                    onPressed: () => selectTime(context),
-                    child: Text(
-                      MaterialLocalizations.of(context).formatTimeOfDay(
-                          selectedTime,
-                          alwaysUse24HourFormat: true),
-                      style: Theme.of(context).textTheme.bodyMedium,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 24),
-              CustomSlider(
-                onChanged: (double newValue) {
-                  selectedMood = newValue.toInt();
-                },
-              ),
-              const SizedBox(height: 14),
-              Emotions(),
-              const SizedBox(height: 24),
-              TextField(
-                controller: _titleController,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                  ),
-                  hintText: 'Title',
-                  hintStyle: Theme.of(context).textTheme.titleMedium,
-                  contentPadding: EdgeInsets.zero,
-                  isDense: true,
                 ),
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              const SizedBox(height: 10),
-              TextField(
-                controller: _descriptionController,
-                decoration: InputDecoration(
-                  border: const OutlineInputBorder(
-                    borderSide: BorderSide.none,
+                const Text(' - '),
+                TextButton(
+                  onPressed: () => selectTime(context),
+                  child: Text(
+                    MaterialLocalizations.of(context).formatTimeOfDay(
+                        selectedTime,
+                        alwaysUse24HourFormat: true),
+                    style: Theme.of(context).textTheme.bodyMedium,
                   ),
-                  hintText: 'Description',
-                  hintStyle: Theme.of(context).textTheme.bodyMedium,
-                  contentPadding: EdgeInsets.zero,
-                  isDense: true,
                 ),
-                style: Theme.of(context).textTheme.bodyMedium,
-                maxLines: null,
-                keyboardType: TextInputType.multiline,
-              ),
-              const SizedBox(height: 24),
-              Text(
-                'What was it about?',
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              const SizedBox(height: 10),
-              Tags(onSelectedTagsChanged: updateSelectedTags),
-            ],
-          ),
-          Positioned(
-            bottom: 16.0, // Distance from bottom
-            left: 0,
-            right: 0,
-            child: Center(
-                child: FilledButton(
-              onPressed: () {
-                final emotionProvider =
-                    Provider.of<EmotionProvider>(context, listen: false);
-                final emotionCounts = emotionProvider.selectedEmotionCounts;
-                _databaseService.addEntry(
-                    '${selectedDate.toString().split(' ')[0]}|${MaterialLocalizations.of(context).formatTimeOfDay(selectedTime, alwaysUse24HourFormat: true)}',
-                    selectedMood,
-                    '$emotionCounts',
-                    _titleController.text,
-                    _descriptionController.text,
-                    selectedTags.toString());
+              ],
+            ),
+            const SizedBox(height: 24),
+            CustomSlider(
+              onChanged: (double newValue) {
+                selectedMood = newValue.toInt();
               },
-              style: ButtonStyle(
-                padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
-                  const EdgeInsets.symmetric(horizontal: 60, vertical: 12),
+            ),
+            const SizedBox(height: 14),
+            Emotions(),
+            const SizedBox(height: 24),
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
                 ),
+                hintText: 'Title',
+                hintStyle: Theme.of(context).textTheme.titleMedium,
+                contentPadding: EdgeInsets.zero,
+                isDense: true,
               ),
-              child: const Text('Save'),
-            )),
-          ),
-        ],
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(
+                  borderSide: BorderSide.none,
+                ),
+                hintText: 'Description',
+                hintStyle: Theme.of(context).textTheme.bodyMedium,
+                contentPadding: EdgeInsets.zero,
+                isDense: true,
+              ),
+              style: Theme.of(context).textTheme.bodyMedium,
+              maxLines: null,
+              keyboardType: TextInputType.multiline,
+            ),
+            const SizedBox(height: 24),
+            Text(
+              'What was it about?',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            const SizedBox(height: 10),
+            Tags(onSelectedTagsChanged: updateSelectedTags),
+            const SizedBox(height: 24),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: FilledButton(
+                onPressed: () {
+                  final emotionProvider =
+                      Provider.of<EmotionProvider>(context, listen: false);
+                  final emotionCounts = emotionProvider.selectedEmotionCounts;
+                  _databaseService.addEntry(
+                      '${selectedDate.toString().split(' ')[0]}|${MaterialLocalizations.of(context).formatTimeOfDay(selectedTime, alwaysUse24HourFormat: true)}',
+                      selectedMood,
+                      '$emotionCounts',
+                      _titleController.text,
+                      _descriptionController.text,
+                      selectedTags.toString());
+                },
+                style: ButtonStyle(
+                  padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+                    const EdgeInsets.symmetric(horizontal: 60, vertical: 12),
+                  ),
+                ),
+                child: const Text('Save'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
