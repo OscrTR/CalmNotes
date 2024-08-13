4cf05504f8720a7992d61e38189ced036b289c1e
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-08-13 22:33:11 +0200

    row size

--- lib/components/calendar.dart
+++ lib/components/calendar.dart
@@ -1,66 +1,74 @@
 import 'package:calm_notes/colors.dart';
+import 'package:calm_notes/providers/entry_provider.dart'; // Adjust import based on your file structure
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
+import 'package:provider/provider.dart';
 
-class Calendar extends StatefulWidget {
+class Calendar extends StatelessWidget {
   const Calendar({super.key});
 
   @override
-  State<Calendar> createState() => _CalendarState();
-}
+  Widget build(BuildContext context) {
+    // Fetch the EntryProvider from the context
+    final provider = Provider.of<EntryProvider>(context);
+    final startDate = provider.startDate;
+    final endDate = provider.endDate;
+    final daysInMonth = DateTime(startDate.year, startDate.month + 1, 0).day;
 
-class _CalendarState extends State<Calendar> {
-  final int daysInMonth = DateTime.now().month == 2
-      ? (DateTime.now().year % 4 == 0 ? 29 : 28)
-      : [4, 6, 9, 11].contains(DateTime.now().month)
-          ? 30
-          : 31;
+    final firstDayOfRange =
+        DateTime(startDate.year, startDate.month, startDate.day);
+    final lastDayOfRange =
+        endDate.isBefore(DateTime(startDate.year, startDate.month + 1, 0))
+            ? endDate
+            : DateTime(startDate.year, startDate.month + 1, 0);
 
-  List<Widget> _buildCalendarDays() {
-    final firstDayOfMonth =
-        DateTime(DateTime.now().year, DateTime.now().month, 1);
-    final startingWeekday = firstDayOfMonth.weekday %
-        7; // Weekday returns 1 (Monday) to 7 (Sunday), adjust to 0 (Sunday) to 6 (Saturday)
+    final firstDayOfWeek =
+        firstDayOfRange.subtract(Duration(days: firstDayOfRange.weekday % 7));
 
-    List<Widget> dayWidgets = [];
+    final totalDays = lastDayOfRange.difference(firstDayOfWeek).inDays + 1;
 
-    // Empty cells for the days before the first day of the month
-    for (int i = 0; i < startingWeekday; i++) {
-      dayWidgets.add(Container());
-    }
+    final numberOfRows = ((totalDays + 6) / 7).ceil(); // Number of weeks
 
-    // Add the days of the month
-    for (int day = 1; day <= daysInMonth; day++) {
-      dayWidgets.add(
-        Container(
-          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-          decoration: BoxDecoration(
-            color: AppColors.primaryColor.withOpacity(0.1),
-            borderRadius: BorderRadius.circular(90),
-          ),
-          child: Center(
-            child: Text(
-              day.toString(),
-              style: const TextStyle(fontSize: 16, color: Colors.transparent),
+    List<Widget> buildCalendarDays() {
+      List<Widget> dayWidgets = [];
+      for (int i = 0; i < totalDays; i++) {
+        DateTime currentDate = firstDayOfWeek.add(Duration(days: i));
+        bool isInRange =
+            currentDate.isAfter(startDate.subtract(const Duration(days: 1))) &&
+                currentDate.isBefore(endDate.add(const Duration(days: 1)));
+
+        dayWidgets.add(
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            decoration: BoxDecoration(
+              color: isInRange
+                  ? AppColors.primaryColor.withOpacity(0.1)
+                  : Colors.transparent,
+              borderRadius: BorderRadius.circular(90),
+            ),
+            child: Center(
+              child: Text(
+                currentDate.day.toString(),
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.transparent,
+                ),
+              ),
             ),
           ),
-        ),
-      );
+        );
+      }
+
+      return dayWidgets;
     }
 
-    return dayWidgets;
-  }
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
-      height: 250,
       width: 352.7,
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(10),
         color: Colors.white,
       ),
-      padding: const EdgeInsets.only(top: 24, bottom: 14, left: 20, right: 20),
+      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 20, right: 20),
       child: Column(
         children: [
           // Day of the week header
@@ -80,8 +88,8 @@ class _CalendarState extends State<Calendar> {
           // Calendar grid
           LayoutGrid(
             columnSizes: [1.fr, 1.fr, 1.fr, 1.fr, 1.fr, 1.fr, 1.fr],
-            rowSizes: List.generate((daysInMonth / 7).ceil(), (index) => auto),
-            children: _buildCalendarDays(),
+            rowSizes: List.generate(numberOfRows, (index) => auto),
+            children: buildCalendarDays(),
           ),
         ],
       ),
