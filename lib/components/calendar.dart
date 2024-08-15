963b164df7677765722496b5835a91d77fb483c0
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-08-15 19:59:04 +0200

    calendar refactor

--- lib/components/calendar.dart
+++ lib/components/calendar.dart
@@ -1,6 +1,6 @@
 import 'package:calm_notes/colors.dart';
 import 'package:calm_notes/models/entry.dart';
-import 'package:calm_notes/providers/entry_provider.dart'; // Adjust import based on your file structure
+import 'package:calm_notes/providers/entry_provider.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_layout_grid/flutter_layout_grid.dart';
@@ -16,120 +16,23 @@ class Calendar extends StatefulWidget {
 
 class _CalendarState extends State<Calendar> {
   bool _showSmileys = false;
+
   @override
   Widget build(BuildContext context) {
-    // Fetch the EntryProvider from the context
     final provider = Provider.of<EntryProvider>(context);
-    final startDate = provider.startDate;
-    final endDate = provider.endDate;
     final entries = provider.filteredEntries;
 
-    Map<DateTime, int> entryMap = {};
+    Map<DateTime, int> entryMap = _convertEntriesToMap(entries);
 
-    Map<DateTime, int> convertEntriesToMap(List<Entry> entries) {
-      Map<DateTime, double> moodTotalMap = {};
-      Map<DateTime, int> moodCountMap = {};
-
-      // Populate the moodMap with entries
-      for (var entry in entries) {
-        DateTime date = DateTime.parse(entry.date.split('|')[0]);
-        // Accumulate the mood values for each date
-        if (moodTotalMap.containsKey(date)) {
-          moodTotalMap[date] = moodTotalMap[date]! + entry.mood.toDouble();
-          moodCountMap[date] = moodCountMap[date]! + 1;
-        } else {
-          moodTotalMap[date] = entry.mood.toDouble();
-          moodCountMap[date] = 1;
-        }
-      }
-      Map<DateTime, int> moodAverageMap = {};
-
-      for (var date in moodTotalMap.keys) {
-        moodAverageMap[date] =
-            (moodTotalMap[date]! / moodCountMap[date]!).round();
-      }
-
-      return moodAverageMap;
-    }
-
-    entryMap = convertEntriesToMap(entries);
-
-    final firstDayOfRange =
-        DateTime(startDate.year, startDate.month, startDate.day);
+    final firstDayOfRange = _getFirstDayOfRange(provider.startDate);
     final lastDayOfRange =
-        endDate.isBefore(DateTime(startDate.year, startDate.month + 1, 0))
-            ? endDate
-            : DateTime(startDate.year, startDate.month + 1, 0);
+        _getLastDayOfRange(provider.startDate, provider.endDate);
 
-    final firstDayOfWeek =
-        firstDayOfRange.subtract(Duration(days: firstDayOfRange.weekday % 7));
-
+    final firstDayOfWeek = _getFirstDayOfWeek(firstDayOfRange);
     final totalDays = lastDayOfRange.difference(firstDayOfWeek).inDays + 1;
+    final numberOfRows = ((totalDays + 6) / 7).ceil();
 
-    final numberOfRows = ((totalDays + 6) / 7).ceil(); // Number of weeks
-
-    List<Widget> buildCalendarDays() {
-      List<Widget> dayWidgets = [];
-      for (int i = 0; i < totalDays; i++) {
-        DateTime currentDate = firstDayOfWeek.add(Duration(days: i));
-        bool isInRange =
-            currentDate.isAfter(startDate.subtract(const Duration(days: 1))) &&
-                currentDate.isBefore(endDate.add(const Duration(days: 1)));
-
-        Color currentDateColor = entryMap.containsKey(currentDate)
-            ? moodColors[entryMap[currentDate]!]
-            : CustomColors.primaryColor.withOpacity(0.1);
-
-        dayWidgets.add(
-          Container(
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-            decoration: BoxDecoration(
-              color: isInRange ? currentDateColor : Colors.transparent,
-              borderRadius: BorderRadius.circular(90),
-            ),
-            child: Center(
-                child: Stack(
-              children: [
-                const SizedBox(
-                  height: 24.7,
-                  width: 24.7,
-                ),
-                if (isInRange &&
-                    entryMap.containsKey(currentDate) &&
-                    _showSmileys)
-                  SvgPicture.asset(
-                    'assets/images/mood${entryMap[currentDate]}.svg',
-                    height: 24.7,
-                    width: 24.7,
-                  )
-              ],
-            )),
-          ),
-        );
-      }
-
-      return dayWidgets;
-    }
-
-    final Locale currentLocale = context.locale;
-    List<String> localizedDays = [];
-    DateFormat dayFormat = DateFormat.E(currentLocale.toString());
-    for (int i = 0; i < 7; i++) {
-      DateTime date = DateTime(2023, 8, 13 + i);
-      String dayAbbreviation = dayFormat.format(date);
-
-      // Remove trailing dot if present
-      if (dayAbbreviation.endsWith('.')) {
-        dayAbbreviation =
-            dayAbbreviation.substring(0, dayAbbreviation.length - 1);
-      }
-
-      // Capitalize the first letter and make the rest lowercase
-      String formattedDay = dayAbbreviation.substring(0, 1).toUpperCase() +
-          dayAbbreviation.substring(1).toLowerCase();
-
-      localizedDays.add(formattedDay);
-    }
+    final localizedDays = _getLocalizedDays(context);
 
     return GestureDetector(
       onTap: () {
@@ -143,38 +46,130 @@ class _CalendarState extends State<Calendar> {
           borderRadius: BorderRadius.circular(10),
           color: Colors.white,
         ),
-        padding:
-            const EdgeInsets.only(top: 24, bottom: 24, left: 20, right: 20),
+        padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 20),
         child: Column(
           children: [
-            // Day of the week header
-            LayoutGrid(
-                columnSizes: [1.fr, 1.fr, 1.fr, 1.fr, 1.fr, 1.fr, 1.fr],
-                rowSizes: const [auto],
-                children: localizedDays
-                    .map(
-                      (day) => Container(
-                        width: 44.68,
-                        alignment: Alignment.center,
-                        child: Text(
-                          day,
-                          style: const TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: CustomColors.ternaryColor),
-                        ),
-                      ),
-                    )
-                    .toList()),
+            _buildDayOfWeekHeader(localizedDays),
             const SizedBox(height: 14),
-            // Calendar grid
-            LayoutGrid(
-              columnSizes: [1.fr, 1.fr, 1.fr, 1.fr, 1.fr, 1.fr, 1.fr],
-              rowSizes: List.generate(numberOfRows, (index) => auto),
-              children: buildCalendarDays(),
-            ),
+            _buildCalendarGrid(entryMap, totalDays, firstDayOfWeek,
+                provider.startDate, provider.endDate, numberOfRows),
           ],
         ),
       ),
+    );
+  }
+
+  Map<DateTime, int> _convertEntriesToMap(List<Entry> entries) {
+    Map<DateTime, double> moodTotalMap = {};
+    Map<DateTime, int> moodCountMap = {};
+
+    for (var entry in entries) {
+      DateTime date = DateTime.parse(entry.date.split('|')[0]);
+      if (moodTotalMap.containsKey(date)) {
+        moodTotalMap[date] = moodTotalMap[date]! + entry.mood.toDouble();
+        moodCountMap[date] = moodCountMap[date]! + 1;
+      } else {
+        moodTotalMap[date] = entry.mood.toDouble();
+        moodCountMap[date] = 1;
+      }
+    }
+
+    return moodTotalMap.map((date, totalMood) =>
+        MapEntry(date, (totalMood / moodCountMap[date]!).round()));
+  }
+
+  DateTime _getFirstDayOfRange(DateTime startDate) {
+    return DateTime(startDate.year, startDate.month, startDate.day);
+  }
+
+  DateTime _getLastDayOfRange(DateTime startDate, DateTime endDate) {
+    return endDate.isBefore(DateTime(startDate.year, startDate.month + 1, 0))
+        ? endDate
+        : DateTime(startDate.year, startDate.month + 1, 0);
+  }
+
+  DateTime _getFirstDayOfWeek(DateTime firstDayOfRange) {
+    return firstDayOfRange
+        .subtract(Duration(days: firstDayOfRange.weekday % 7));
+  }
+
+  List<String> _getLocalizedDays(BuildContext context) {
+    final Locale currentLocale = context.locale;
+    DateFormat dayFormat = DateFormat.E(currentLocale.toString());
+
+    return List.generate(7, (i) {
+      DateTime date = DateTime(2023, 8, 13 + i);
+      String dayAbbreviation = dayFormat.format(date).replaceAll('.', '');
+      return dayAbbreviation.substring(0, 1).toUpperCase() +
+          dayAbbreviation.substring(1).toLowerCase();
+    });
+  }
+
+  Widget _buildDayOfWeekHeader(List<String> localizedDays) {
+    return LayoutGrid(
+      columnSizes: List.filled(7, 1.fr),
+      rowSizes: const [auto],
+      children: localizedDays
+          .map(
+            (day) => Container(
+              width: 44.68,
+              alignment: Alignment.center,
+              child: Text(
+                day,
+                style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: CustomColors.ternaryColor,
+                ),
+              ),
+            ),
+          )
+          .toList(),
+    );
+  }
+
+  Widget _buildCalendarGrid(
+      Map<DateTime, int> entryMap,
+      int totalDays,
+      DateTime firstDayOfWeek,
+      DateTime startDate,
+      DateTime endDate,
+      int numberOfRows) {
+    return LayoutGrid(
+      columnSizes: List.filled(7, 1.fr),
+      rowSizes: List.generate(numberOfRows, (index) => auto),
+      children: List.generate(totalDays, (i) {
+        DateTime currentDate = firstDayOfWeek.add(Duration(days: i));
+        bool isInRange =
+            currentDate.isAfter(startDate.subtract(const Duration(days: 1))) &&
+                currentDate.isBefore(endDate.add(const Duration(days: 1)));
+
+        Color currentDateColor = entryMap.containsKey(currentDate)
+            ? moodColors[entryMap[currentDate]!]
+            : CustomColors.primaryColor.withOpacity(0.1);
+
+        return Container(
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          decoration: BoxDecoration(
+            color: isInRange ? currentDateColor : Colors.transparent,
+            borderRadius: BorderRadius.circular(90),
+          ),
+          child: Center(
+            child: Stack(
+              children: [
+                const SizedBox(height: 24.7, width: 24.7),
+                if (isInRange &&
+                    entryMap.containsKey(currentDate) &&
+                    _showSmileys)
+                  SvgPicture.asset(
+                    'assets/images/mood${entryMap[currentDate]}.svg',
+                    height: 24.7,
+                    width: 24.7,
+                  )
+              ],
+            ),
+          ),
+        );
+      }),
     );
   }
 }
