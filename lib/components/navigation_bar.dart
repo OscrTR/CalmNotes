e84a9b336f1b3a9d2e4ad563219c74bf73a9cc8f
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-08-16 10:54:36 +0200

    refactor navigation

--- lib/components/navigation_bar.dart
+++ lib/components/navigation_bar.dart
@@ -3,46 +3,62 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:material_symbols_icons/material_symbols_icons.dart';
 
-class CustomNavigationBar extends StatefulWidget {
+class CustomNavigationBar extends StatelessWidget {
   const CustomNavigationBar({super.key});
 
   @override
-  State<CustomNavigationBar> createState() => _CustomNavigationBarState();
-}
-
-class _CustomNavigationBarState extends State<CustomNavigationBar> {
-  @override
   Widget build(BuildContext context) {
+    final List<_NavItem> navItems = [
+      _NavItem(
+        icon: const Icon(
+          Symbols.library_books,
+          weight: 300,
+        ),
+        route: '/',
+        iconSize: 30,
+      ),
+      _NavItem(
+        icon: const Icon(Icons.add_circle),
+        route: '/entry',
+        iconSize: 44,
+      ),
+      _NavItem(
+        icon: const Icon(
+          Symbols.analytics,
+          weight: 300,
+        ),
+        route: '/statistics',
+        iconSize: 30,
+      ),
+    ];
+
     return SizedBox(
       height: 80,
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          IconButton(
-              iconSize: 30,
-              color: CustomColors.primaryColor,
-              onPressed: () => GoRouter.of(context).push('/'),
-              icon: const Icon(
-                Symbols.library_books,
-                weight: 300,
-              )),
-          IconButton(
-              iconSize: 44,
-              color: CustomColors.primaryColor,
-              onPressed: () {
-                GoRouter.of(context).push('/entry');
-              },
-              icon: const Icon(Icons.add_circle)),
-          IconButton(
-              iconSize: 30,
-              color: CustomColors.primaryColor,
-              onPressed: () => GoRouter.of(context).push('/statistics'),
-              icon: const Icon(
-                Symbols.analytics,
-                weight: 300,
-              )),
-        ],
+        children: navItems.map((item) => _buildNavItem(context, item)).toList(),
       ),
     );
   }
+
+  Widget _buildNavItem(BuildContext context, _NavItem item) {
+    return IconButton(
+      iconSize: item.iconSize,
+      color: CustomColors.primaryColor,
+      onPressed: () => GoRouter.of(context).push(item.route),
+      icon: item.icon,
+    );
+  }
+}
+
+class _NavItem {
+  final Icon icon;
+  final String route;
+  final double iconSize;
+
+  _NavItem({
+    required this.icon,
+    required this.route,
+    required this.iconSize,
+  });
 }
