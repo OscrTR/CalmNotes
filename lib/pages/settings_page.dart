e033abe4c4e85b89c05ccc738d6e3c8b152df21e
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-08-16 12:08:53 +0200

    settings refactor

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,5 @@
 import 'package:calm_notes/colors.dart';
+import 'package:calm_notes/models/reminder.dart';
 import 'package:calm_notes/providers/reminder_provider.dart';
 import 'package:calm_notes/services/notification_service.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -16,7 +17,178 @@ class SettingsPage extends StatefulWidget {
 class _SettingsPageState extends State<SettingsPage> {
   TimeOfDay selectedTime = TimeOfDay.now();
 
-  Future<void> selectTime(BuildContext context, VoidCallback onSuccess) async {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Text(
+            context.tr('settings_title'),
+            style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          const SizedBox(height: 24),
+          _buildRemindersSection(context),
+          const SizedBox(height: 24),
+          _buildLanguageSection(context),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRemindersSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          context.tr('settings_reminders'),
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        const SizedBox(height: 10),
+        Consumer<ReminderProvider>(
+          builder: (context, provider, child) {
+            final reminders = provider.reminders;
+            if (reminders.isEmpty) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+                child: Text(context.tr('settings_reminders_not_found')),
+              );
+            }
+            return _buildRemindersList(reminders, provider);
+          },
+        ),
+        const SizedBox(height: 10),
+        FilledButton(
+          onPressed: () => _selectTime(context),
+          child: Text(context.tr('settings_reminders_add')),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRemindersList(
+      List<Reminder> reminders, ReminderProvider provider) {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: reminders.length,
+      itemBuilder: (context, index) {
+        final reminder = reminders[index];
+        return Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              reminder.time,
+              style: Theme.of(context).textTheme.bodyMedium,
+            ),
+            GestureDetector(
+              onTap: () => provider.deleteReminder(reminder.id!),
+              child: Container(
+                height: 30,
+                width: 30,
+                alignment: Alignment.centerRight,
+                child: const ClipRect(
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    widthFactor: 0.85,
+                    child: Icon(
+                      Symbols.delete,
+                      color: CustomColors.primaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildLanguageSection(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          context.tr('settings_language'),
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        TextButton(
+          onPressed: () => _showLanguageDialog(context),
+          child: Text(
+            _getLanguageName(context.locale),
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildLanguageOption(BuildContext context,
+      {required String languageCode,
+      required String countryCode,
+      required String label}) {
+    return OutlinedButton(
+      onPressed: () {
+        Navigator.pop(context, 'Language selection');
+        context.setLocale(Locale(languageCode, countryCode));
+      },
+      child: Text(label),
+    );
+  }
+
+  Widget _buildLanguageDialogContent(BuildContext context) {
+    return Wrap(
+      spacing: 10,
+      children: [
+        buildLanguageOption(
+          context,
+          languageCode: 'en',
+          countryCode: 'US',
+          label: context.tr('settings_language_dialog_english'),
+        ),
+        buildLanguageOption(
+          context,
+          languageCode: 'fr',
+          countryCode: 'FR',
+          label: context.tr('settings_language_dialog_french'),
+        ),
+      ],
+    );
+  }
+
+  String _getLanguageName(Locale locale) {
+    switch (locale.languageCode) {
+      case 'en':
+        return 'English';
+      case 'fr':
+        return 'Français';
+      default:
+        return locale.languageCode;
+    }
+  }
+
+  void _showLanguageDialog(BuildContext context) {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        backgroundColor: CustomColors.backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        title: Text(context.tr('settings_language_dialog_title')),
+        content: _buildLanguageDialogContent(context),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: Text(context.tr('global_dialog_cancel')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
     final TimeOfDay? picked = await showTimePicker(
       context: context,
       initialTime: selectedTime,
@@ -30,156 +202,19 @@ class _SettingsPageState extends State<SettingsPage> {
     if (picked != null) {
       setState(() {
         selectedTime = picked;
-        onSuccess.call();
+      });
+      Future.microtask(() {
+        _addReminder(context);
       });
     }
   }
 
-  String getLanguageName(Locale locale) {
-    switch (locale.languageCode) {
-      case 'en':
-        return 'English';
-      case 'fr':
-        return 'Français';
-      default:
-        return locale.languageCode;
-    }
+  void _addReminder(BuildContext context) {
+    final reminderProvider =
+        Provider.of<ReminderProvider>(context, listen: false);
+    final formattedTime = MaterialLocalizations.of(context)
+        .formatTimeOfDay(selectedTime, alwaysUse24HourFormat: true);
+    reminderProvider.addReminder(formattedTime);
+    NotificationService.showNotification(selectedTime);
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Text(
-            context.tr('settings_title'),
-            style: Theme.of(context).textTheme.headlineMedium,
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Text(
-            context.tr('settings_reminders'),
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          Consumer<ReminderProvider>(
-            builder: (context, provider, child) {
-              final reminders = provider.reminders;
-              if (reminders.isEmpty) {
-                return Column(children: [
-                  const SizedBox(height: 10),
-                  Text(context.tr('settings_reminders_not_found')),
-                  const SizedBox(height: 10),
-                ]);
-              }
-              final double height = reminders.length * 40.0;
-              return SizedBox(
-                height: height,
-                child: ListView.builder(
-                  itemCount: reminders.length,
-                  itemBuilder: (context, index) {
-                    final reminder = reminders[index];
-                    return Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          reminder.time,
-                          style: Theme.of(context).textTheme.bodyMedium,
-                        ),
-                        IconButton(
-                          onPressed: () {
-                            provider.deleteReminder(reminder.id!);
-                          },
-                          style: const ButtonStyle(
-                            tapTargetSize: MaterialTapTargetSize.shrinkWrap,
-                          ),
-                          icon: const Icon(
-                            Symbols.delete,
-                          ),
-                        ),
-                      ],
-                    );
-                  },
-                ),
-              );
-            },
-          ),
-          FilledButton(
-            onPressed: () {
-              selectTime(context, () {
-                if (!mounted) return;
-                final reminderProvider =
-                    Provider.of<ReminderProvider>(context, listen: false);
-                reminderProvider.addReminder(
-                  MaterialLocalizations.of(context).formatTimeOfDay(
-                      selectedTime,
-                      alwaysUse24HourFormat: true),
-                );
-                NotificationService.showNotification(selectedTime);
-              });
-            },
-            child: Text(context.tr('settings_reminders_add')),
-          ),
-          const SizedBox(
-            height: 24,
-          ),
-          Text(
-            context.tr('settings_language'),
-            style: Theme.of(context).textTheme.titleMedium,
-          ),
-          TextButton(
-            onPressed: () {
-              _showLanguageDialog(context);
-            },
-            child: Text(
-              getLanguageName(context.locale),
-              style: Theme.of(context).textTheme.bodyMedium,
-            ),
-          )
-        ],
-      ),
-    );
-  }
-}
-
-void _showLanguageDialog(BuildContext context) {
-  showDialog<String>(
-    context: context,
-    builder: (BuildContext context) => AlertDialog(
-      backgroundColor: CustomColors.backgroundColor,
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(5),
-      ),
-      title: Text(context.tr('settings_language_dialog_title')),
-      content: _buildLanguageDialogContent(context),
-      actions: <Widget>[
-        TextButton(
-          onPressed: () => Navigator.pop(context, 'Cancel'),
-          child: Text(context.tr('global_dialog_cancel')),
-        ),
-      ],
-    ),
-  );
-}
-
-Widget _buildLanguageDialogContent(BuildContext context) {
-  return Wrap(
-    spacing: 10,
-    children: [
-      OutlinedButton(
-          onPressed: () {
-            Navigator.pop(context, 'Language selection');
-            context.setLocale(const Locale('en', 'US'));
-          },
-          child: Text(context.tr('settings_language_dialog_english'))),
-      OutlinedButton(
-          onPressed: () {
-            Navigator.pop(context, 'Language selection');
-            context.setLocale(const Locale('fr', 'FR'));
-          },
-          child: Text(context.tr('settings_language_dialog_french'))),
-    ],
-  );
 }
