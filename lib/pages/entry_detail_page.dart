a298cecf3491ee7ca342900da65f73c7fc510f88
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-08-16 11:41:55 +0200

    entry detail refactor

--- lib/pages/entry_detail_page.dart
+++ lib/pages/entry_detail_page.dart
@@ -24,12 +24,10 @@ class EntryDetailPage extends StatefulWidget {
 
 class _EntryDetailPageState extends State<EntryDetailPage> {
   final DatabaseService _databaseService = DatabaseService.instance;
-  Entry? _savedEntry;
 
   DateTime _selectedDate = DateTime.now();
   TimeOfDay _selectedTime = TimeOfDay.now();
   int? _selectedMood;
-  late Future<int?> _futureMood;
 
   final TextEditingController _titleController = TextEditingController();
   final TextEditingController _descriptionController = TextEditingController();
@@ -44,94 +42,7 @@ class _EntryDetailPageState extends State<EntryDetailPage> {
   @override
   void initState() {
     super.initState();
-    _futureMood = _databaseService.getEntryMood(widget.entryId);
-    // Initialize controllers or other variables if needed
-    _fetchData();
-  }
-
-  Future<void> _fetchData() async {
-    final data = await _databaseService.getEntry(widget.entryId);
-    setState(() {
-      _savedEntry = data;
-    });
-    _selectedMood = _savedEntry!.mood;
-    _selectedDate = getDateTime(_savedEntry!.date)!;
-    _selectedTime = parseTimeOfDay(_savedEntry!.date);
-    _titleController.text = _savedEntry!.title!;
-    _descriptionController.text = _savedEntry!.description!;
-    Future.microtask(() {
-      Provider.of<EmotionProvider>(context, listen: false)
-          .setEmotions(widget.entryId);
-      Provider.of<TagProvider>(context, listen: false).setTags(widget.entryId);
-    });
-  }
-
-  DateTime? getDateTime(String date) {
-    try {
-      final parts = date.split('|');
-      final dateTime = DateFormat('yyyy-MM-dd').parse(parts[0]);
-      final time = DateFormat('HH:mm').parse(parts[1]);
-      return DateTime(
-          dateTime.year, dateTime.month, dateTime.day, time.hour, time.minute);
-    } catch (e) {
-      return null;
-    }
-  }
-
-  TimeOfDay parseTimeOfDay(String dateTimeString) {
-    // Split the string to separate date and time
-    List<String> parts = dateTimeString.split('|');
-
-    // Extract the time part (assuming the format is correct)
-    if (parts.length != 2) {
-      throw const FormatException('Invalid date-time format');
-    }
-
-    String timePart = parts[1];
-
-    // Split the time part into hours and minutes
-    List<String> timeParts = timePart.split(':');
-    if (timeParts.length != 2) {
-      throw const FormatException('Invalid time format');
-    }
-
-    int hour = int.parse(timeParts[0]);
-    int minute = int.parse(timeParts[1]);
-
-    // Create and return a TimeOfDay object
-    return TimeOfDay(hour: hour, minute: minute);
-  }
-
-  Future<void> _selectDate(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: _selectedDate,
-      firstDate: DateTime(2023),
-      lastDate: DateTime(2043),
-    );
-    if (picked != null && picked != _selectedDate) {
-      setState(() {
-        _selectedDate = picked;
-      });
-    }
-  }
-
-  Future<void> _selectTime(BuildContext context) async {
-    final TimeOfDay? picked = await showTimePicker(
-      context: context,
-      initialTime: _selectedTime,
-      builder: (BuildContext context, Widget? child) {
-        return MediaQuery(
-          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
-          child: child!,
-        );
-      },
-    );
-    if (picked != null && picked != _selectedTime) {
-      setState(() {
-        _selectedTime = picked;
-      });
-    }
+    _loadEntryData();
   }
 
   @override
@@ -143,29 +54,18 @@ class _EntryDetailPageState extends State<EntryDetailPage> {
           children: [
             _buildHeader(context),
             const SizedBox(height: 24),
-            FutureBuilder(
-                future: _futureMood,
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const CircularProgressIndicator();
-                  } else if (snapshot.hasError) {
-                    return Text('Error: ${snapshot.error}');
-                  } else if (!snapshot.hasData) {
-                    return Text(context.tr('edit_no_data'));
-                  } else {
-                    final initialValue =
-                        _selectedMood ?? snapshot.data!.toDouble();
-                    return CustomSlider(
-                        initialValue: initialValue.toDouble(),
-                        onChanged: (double newValue) {
-                          setState(() {
-                            _selectedMood = newValue.toInt();
-                          });
-                        });
-                  }
-                }),
-            const SizedBox(height: 14),
-            const Emotions(),
+            if (_selectedMood != null) ...[
+              CustomSlider(
+                initialValue: _selectedMood!.toDouble(),
+                onChanged: (double newValue) {
+                  setState(() {
+                    _selectedMood = newValue.toInt();
+                  });
+                },
+              ),
+              const SizedBox(height: 14),
+              const Emotions(),
+            ],
             const SizedBox(height: 24),
             _buildTitleField(context),
             const SizedBox(height: 10),
@@ -181,104 +81,91 @@ class _EntryDetailPageState extends State<EntryDetailPage> {
   }
 
   Widget _buildHeader(BuildContext context) {
-    final Locale currentLocale = context.locale;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                GoRouter.of(context).push('/');
-                Provider.of<EmotionProvider>(context, listen: false)
-                    .resetEmotions();
-                Provider.of<TagProvider>(context, listen: false).resetTags();
-              },
-              child: Container(
-                height: 48,
-                width: 48,
-                alignment: Alignment.centerLeft,
-                child: const ClipRect(
-                  child: Align(
-                    alignment: Alignment.centerRight,
-                    widthFactor: 0.85,
-                    child: Icon(
-                      Icons.arrow_back,
-                      color: CustomColors.primaryColor,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Text(context.tr('edit_page_title'),
-                style: Theme.of(context).textTheme.headlineMedium),
-            GestureDetector(
-              onTap: () => showDialog<String>(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                  backgroundColor: CustomColors.backgroundColor,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5),
-                  ),
-                  title: Text(context.tr('edit_delete_dialog_title')),
-                  content: Text(context.tr('edit_delete_dialog_subtitle')),
-                  actions: <Widget>[
-                    TextButton(
-                      onPressed: () => Navigator.pop(context, 'Cancel'),
-                      child: Text(context.tr('global_dialog_cancel')),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        _databaseService.deleteEntry(widget.entryId);
-                        Navigator.pop(context, 'Delete');
-                        GoRouter.of(context).push('/');
-                        Provider.of<EmotionProvider>(context, listen: false)
-                            .resetEmotions();
-                        Provider.of<TagProvider>(context, listen: false)
-                            .resetTags();
-                      },
-                      child: Text(context.tr('edit_delete_dialog_delete')),
-                    ),
-                  ],
-                ),
-              ),
-              child: Container(
-                height: 48,
-                width: 48,
-                alignment: Alignment.centerRight,
-                child: const SizedBox(
-                  width: 20,
-                  child: Icon(
-                    Symbols.delete,
-                    color: CustomColors.primaryColor,
-                  ),
-                ),
-              ),
-            ),
-          ],
+        _buildTopBar(context),
+        const SizedBox(height: 10),
+        _buildDateTimePickerRow(context),
+      ],
+    );
+  }
+
+  Widget _buildTopBar(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        _buildBackButton(context),
+        Text(
+          context.tr('edit_page_title'),
+          style: Theme.of(context).textTheme.headlineMedium,
         ),
-        Row(
-          children: [
-            TextButton(
-              onPressed: () => _selectDate(context),
-              child: Text(
-                DateFormat('d MMMM yyyy', currentLocale.toString())
-                    .format(_selectedDate),
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
+        _buildDeleteButton(context),
+      ],
+    );
+  }
+
+  Widget _buildBackButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _navigateBack(context),
+      child: Container(
+        height: 48,
+        width: 48,
+        alignment: Alignment.centerLeft,
+        child: const ClipRect(
+          child: Align(
+            alignment: Alignment.centerRight,
+            widthFactor: 0.85,
+            child: Icon(
+              Icons.arrow_back,
+              color: CustomColors.primaryColor,
             ),
-            const Text(' - '),
-            TextButton(
-              onPressed: () => _selectTime(context),
-              child: Text(
-                MaterialLocalizations.of(context).formatTimeOfDay(_selectedTime,
-                    alwaysUse24HourFormat: true),
-                style: Theme.of(context).textTheme.bodyMedium,
-              ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDeleteButton(BuildContext context) {
+    return GestureDetector(
+      onTap: () => _showDeleteDialog(context),
+      child: Container(
+        height: 48,
+        width: 48,
+        alignment: Alignment.centerRight,
+        child: const SizedBox(
+          width: 20,
+          child: Icon(
+            Symbols.delete,
+            color: CustomColors.primaryColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildDateTimePickerRow(BuildContext context) {
+    final Locale currentLocale = context.locale;
+    return Row(
+      children: [
+        TextButton(
+          onPressed: () => _selectDate(context),
+          child: Text(
+            DateFormat('d MMMM yyyy', currentLocale.toString())
+                .format(_selectedDate),
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ),
+        const Text(' - '),
+        TextButton(
+          onPressed: () => _selectTime(context),
+          child: Text(
+            MaterialLocalizations.of(context).formatTimeOfDay(
+              _selectedTime,
+              alwaysUse24HourFormat: true,
             ),
-          ],
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
         ),
       ],
     );
@@ -326,6 +213,95 @@ class _EntryDetailPageState extends State<EntryDetailPage> {
     );
   }
 
+  Widget _buildSaveButton(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: FilledButton(
+        onPressed: () => _saveEntry(context),
+        style: ButtonStyle(
+          padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
+            const EdgeInsets.symmetric(horizontal: 60, vertical: 12),
+          ),
+        ),
+        child: Text(context.tr('edit_save')),
+      ),
+    );
+  }
+
+  void _navigateBack(BuildContext context) {
+    GoRouter.of(context).push('/');
+    Provider.of<EmotionProvider>(context, listen: false).resetEmotions();
+    Provider.of<TagProvider>(context, listen: false).resetTags();
+  }
+
+  void _showDeleteDialog(BuildContext context) {
+    showDialog<String>(
+      context: context,
+      builder: (BuildContext context) => AlertDialog(
+        backgroundColor: CustomColors.backgroundColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5),
+        ),
+        title: Text(context.tr('edit_delete_dialog_title')),
+        content: Text(context.tr('edit_delete_dialog_subtitle')),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.pop(context, 'Cancel'),
+            child: Text(context.tr('global_dialog_cancel')),
+          ),
+          TextButton(
+            onPressed: () => _deleteEntry(context),
+            child: Text(context.tr('edit_delete_dialog_delete')),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _deleteEntry(BuildContext context) {
+    _databaseService.deleteEntry(widget.entryId);
+    Navigator.pop(context, 'Delete');
+    _navigateBack(context);
+  }
+
+  void _saveEntry(BuildContext context) {
+    if (_selectedMood != null) {
+      final entry = Entry(
+        id: widget.entryId,
+        date: _formatDateTime(context),
+        mood: _selectedMood!,
+        emotions: _convertEmotionsToString(
+            Provider.of<EmotionProvider>(context, listen: false)
+                .emotionsToDisplay),
+        title: _titleController.text,
+        description: _descriptionController.text,
+        tags: _convertTagsToString(
+            Provider.of<TagProvider>(context, listen: false).tagsToDisplay),
+      );
+
+      _databaseService.updateEntry(entry);
+      _navigateBack(context);
+    }
+  }
+
+  String _formatDateTime(BuildContext context) {
+    return '${_selectedDate.toString().split(' ')[0]}|${MaterialLocalizations.of(context).formatTimeOfDay(_selectedTime, alwaysUse24HourFormat: true)}';
+  }
+
+  String _convertEmotionsToString(List<Emotion> emotions) {
+    return emotions
+        .where((emotion) => emotion.selectedCount > 0)
+        .map((emotion) => '${emotion.name} : ${emotion.selectedCount}')
+        .join(', ');
+  }
+
+  String _convertTagsToString(List<Tag> tags) {
+    return tags
+        .where((tag) => tag.selectedCount > 0)
+        .map((tag) => '${tag.name} : ${tag.selectedCount}')
+        .join(', ');
+  }
+
   String convertEmotionsToString(List<Emotion> emotions) {
     final filteredEmotions =
         emotions.where((emotion) => emotion.selectedCount > 0);
@@ -345,42 +321,69 @@ class _EntryDetailPageState extends State<EntryDetailPage> {
     return emotionStrings.join(', ');
   }
 
-  Widget _buildSaveButton(BuildContext context) {
-    return Align(
-      alignment: Alignment.bottomCenter,
-      child: FilledButton(
-        onPressed: () {
-          final emotionProvider =
-              Provider.of<EmotionProvider>(context, listen: false);
-          final emotionCounts =
-              convertEmotionsToString(emotionProvider.emotionsToDisplay);
+  Future<void> _loadEntryData() async {
+    try {
+      final entry = await _databaseService.getEntry(widget.entryId);
+      setState(() {
+        _selectedMood = entry.mood;
+        _selectedDate = _parseDate(entry.date);
+        _selectedTime = _parseTime(entry.date);
+        _titleController.text = entry.title ?? '';
+        _descriptionController.text = entry.description ?? '';
+      });
 
-          final tagProvider = Provider.of<TagProvider>(context, listen: false);
-          final tagCounts = convertTagsToString(tagProvider.tagsToDisplay);
+      Future.microtask(() {
+        Provider.of<EmotionProvider>(context, listen: false)
+            .setEmotions(widget.entryId);
+        Provider.of<TagProvider>(context, listen: false)
+            .setTags(widget.entryId);
+      });
+    } catch (e) {
+      // Handle error appropriately
+      print("Error loading entry data: $e");
+    }
+  }
 
-          final entry = Entry(
-            id: widget.entryId,
-            date:
-                '${_selectedDate.toString().split(' ')[0]}|${MaterialLocalizations.of(context).formatTimeOfDay(_selectedTime, alwaysUse24HourFormat: true)}',
-            mood: _selectedMood!,
-            emotions: emotionCounts,
-            title: _titleController.text,
-            description: _descriptionController.text,
-            tags: tagCounts,
-          );
+  DateTime _parseDate(String date) {
+    final parts = date.split('|');
+    final dateTime = DateFormat('yyyy-MM-dd').parse(parts[0]);
+    return dateTime;
+  }
 
-          _databaseService.updateEntry(entry);
-          GoRouter.of(context).push('/');
-          emotionProvider.resetEmotions();
-          tagProvider.resetTags();
-        },
-        style: ButtonStyle(
-          padding: WidgetStateProperty.all<EdgeInsetsGeometry>(
-            const EdgeInsets.symmetric(horizontal: 60, vertical: 12),
-          ),
-        ),
-        child: Text(context.tr('edit_save')),
-      ),
+  TimeOfDay _parseTime(String date) {
+    final parts = date.split('|')[1].split(':');
+    return TimeOfDay(hour: int.parse(parts[0]), minute: int.parse(parts[1]));
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final picked = await showDatePicker(
+      context: context,
+      initialDate: _selectedDate,
+      firstDate: DateTime(2023),
+      lastDate: DateTime(2043),
     );
+    if (picked != null) {
+      setState(() {
+        _selectedDate = picked;
+      });
+    }
+  }
+
+  Future<void> _selectTime(BuildContext context) async {
+    final picked = await showTimePicker(
+      context: context,
+      initialTime: _selectedTime,
+      builder: (context, child) {
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(alwaysUse24HourFormat: true),
+          child: child!,
+        );
+      },
+    );
+    if (picked != null) {
+      setState(() {
+        _selectedTime = picked;
+      });
+    }
   }
 }
