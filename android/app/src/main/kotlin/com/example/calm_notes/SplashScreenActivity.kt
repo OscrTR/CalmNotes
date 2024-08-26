ea5425bdc4e8be50938cb4907bad841a2eee8284
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-08-26 15:48:17 +0200

    remove splash logo

--- android/app/src/main/kotlin/com/example/calm_notes/SplashScreenActivity.kt
+++ android/app/src/main/kotlin/com/example/calm_notes/SplashScreenActivity.kt
@@ -1,4 +1,22 @@
 package com.example.calm_notes
 
-class SplashScreenActivity {
+// Import FlutterActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.flutter.embedding.android.FlutterActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+        // Start FlutterActivity
+        startActivity(
+            FlutterActivity.createDefaultIntent(this@SplashScreenActivity)
+        )
+
+
+        // Finish the splash screen activity
+        finish()
+    }
 }
