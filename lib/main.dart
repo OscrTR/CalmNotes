27c793eb686abfba5f4ed1da90544ed38f0e3e75
Oscar Thiébaut-Rizzoni <oscar.thiebaut@proton.me>
2024-09-27 12:10:09 +0200

    remove db checks

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:calm_notes/providers/emotion_provider.dart';
 import 'package:calm_notes/providers/entry_provider.dart';
 import 'package:calm_notes/providers/reminder_provider.dart';
 import 'package:calm_notes/providers/tag_provider.dart';
-import 'package:calm_notes/services/database_service.dart';
 import 'package:calm_notes/services/notification_service.dart';
 import 'package:calm_notes/theme.dart';
 import 'package:easy_localization/easy_localization.dart';
@@ -11,14 +10,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'router.dart';
 
-final DatabaseService _databaseService = DatabaseService.instance;
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await NotificationService.initializeNotifications();
-  await _databaseService.checkIfColumnExists();
-  await _databaseService.convertOldEntries();
 
   runApp(
     EasyLocalization(
